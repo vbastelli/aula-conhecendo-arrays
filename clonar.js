4478ad392603d55cdc59ed6799145a338c98690c
Victório <victoriobastelli@gmail.com>
2024-08-25 14:03:25 -0300

    segundo commit

--- clonar.js
+++ clonar.js
@@ -1,8 +1,8 @@
 const notas = [7, 7, 8, 9];
-const novaListaNotas = [...notas];
+const novaListaNotas = [...notas, 10];
 
 // spread operator
-novaListaNotas.push(10)
+// novaListaNotas.push(10)
 
 
 // const nota = 9.75; 
